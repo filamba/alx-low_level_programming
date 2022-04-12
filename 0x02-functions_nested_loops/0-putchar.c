38f10c86ccbaeb65617108d0702bcfc72e2c33aa
filamba <Bhaylay@gmail.com>
2022-04-12 05:04:34 -0700

    trial 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,19 @@
-include <unistd.h>
-
+include "main.h"
 /**
- *  _putchar -writes the character c to stdout
- *  @c: the c the character to print
- *
- *  return: on success 1
- *  on error, -1 is returned, and errno is set appropriately
- */
-int _putchar(char c)
+ * main - entry block
+ * @void: no argument
+ * return: 0
+ **/
+int main(void)
 {
-	| return (write(1, &c, 1));
+	char ermiaas[] = "_putchar";
+
+	int c;
+
+	for (c = 0; c< 8; c++)
+	{
+		_putchar(ermias[c]);
+	}
+	_putchar('\n');
+	return (0);
 }
