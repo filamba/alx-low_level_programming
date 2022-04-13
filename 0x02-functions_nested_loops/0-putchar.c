b3fafff487a0720bf9234f95ded62c0c95edcb1a
filamba <Bhaylay@gmail.com>
2022-04-13 02:12:32 -0700

    new

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-<<<<<<< HEAD
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -19,17 +19,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-=======
-	char ermias[] = "_putchar";
-
-	int c;
-
-	for (c = 0; c< 8; c++)
-	{
-		_putchar(ermias[c]);
-	}
->>>>>>> 5c607898030005f67e4b3c34b6444194f872b7b8
 	_putchar('\n');
-	
+
 	return (0);
 }
