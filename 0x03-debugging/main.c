009342f6756213f5e9571f174f2969f438b91197
filamba <bhaylay@gmail.com>
2022-04-14 03:05:25 -0700

    trial 1

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests function thst prints if integer is posetive or negative
+ * return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i=98;
+	posetive_or_negative(i);
+
+	return (0);
+}
